116b290679bea743fab99b99670c8f123612ca37
MiraneKazuya <bitay1820468@mkt.ceu.edu.ph>
2019-01-22 15:30:16 +0800

    Simple Game Movement

---
+++ MainFrame.java
@@ -0,0 +1,53 @@
+import javax.swing.JFrame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MainFrame extends JFrame implements KeyListener{
+
+	 MainDraw gameDraw;
+
+	public MainFrame(){
+			this.gameDraw = new MainDraw();
+	}
+
+	public void keyPressed(KeyEvent e){
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			gameDraw.moveRight();
+			System.out.println("Position: " + gameDraw.x + ", " + gameDraw.y);
+		}
+
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			gameDraw.moveLeft();
+			System.out.println("Position: " + gameDraw.x + ", " + gameDraw.y);
+		}
+
+		else if(e.getKeyCode() == KeyEvent.VK_UP){
+			gameDraw.moveUp();
+			System.out.println("Position: " + gameDraw.x + ", " + gameDraw.y);
+		}
+
+		else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			gameDraw.moveDown();
+			System.out.println("Position: " + gameDraw.x + ", " + gameDraw.y);
+		}
+	}
+
+	public void keyReleased(KeyEvent e){
+		
+	}
+
+	public void keyTyped(KeyEvent e){
+
+	}
+
+	public static void main(String args[]){
+		MainFrame gameFrame = new MainFrame();
+		gameFrame.setSize(600,600);
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameFrame.setVisible(true);
+		gameFrame.getContentPane().add(gameFrame.gameDraw);
+		gameFrame.addKeyListener(gameFrame);
+		System.out.println("BRUH");
+	}
+
+}
